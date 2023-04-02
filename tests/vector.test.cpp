300081bc633ab47be137ed938092177f898df300
ARtemachka <artemkababuh@gmail.com>
2023-04-02 15:59:19 +0200

    Add VectorTest.Construction unit test

--- tests/vector.test.cpp
+++ tests/vector.test.cpp
@@ -1,7 +1,38 @@
 #include "cstm/vector.hpp"
 #include <gtest/gtest.h>
 
-int main()
+#include <algorithm>
+#include <limits>
+
+TEST(VectorTest, Construction)
 {
-    return 0;
+    {
+        cstm::vector v;
+
+        EXPECT_TRUE(v.size() == 0);
+        EXPECT_TRUE(v.data() == nullptr);
+    }
+
+    {
+        cstm::vector v(0);
+
+        EXPECT_TRUE(v.size() == 0);
+        EXPECT_TRUE(v.data() == nullptr);
+    }
+
+    {
+        cstm::vector v(10);
+
+        EXPECT_TRUE(v.size() == 10);
+        EXPECT_TRUE(v.data() != nullptr);
+        EXPECT_TRUE(std::all_of(v.data(), v.data() + v.size(), [](int n) { return n == 0; }));
+    }
+
+    {
+        cstm::vector v(10, 5);
+
+        EXPECT_TRUE(v.size() == 10);
+        EXPECT_TRUE(v.data() != nullptr);
+        EXPECT_TRUE(std::all_of(v.data(), v.data() + v.size(), [](int n) { return n == 5; }));
+    }
 }
